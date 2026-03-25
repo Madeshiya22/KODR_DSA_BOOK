8a606d131f97c551f026cd119b47f4a5bd2f130c
Rahul Madeshiya <rahulmadeshiya851@gmail.com>
2026-03-25 19:45:59 +0530

    sloving a question 53 v pattern

---
+++ Sheet_1/Q53/DSA.js
@@ -0,0 +1,20 @@
+// V pattern
+import promptSync from "prompt-sync";
+const prompt = promptSync();
+// let n = Number(prompt("Enter the number of rows: "));
+// for (let i = 1; i <= n; i++) {
+//   for (let j = 1; j <= n+4 ; j++) {
+//     if (i == j || i + j === n + 4) process.stdout.write("* ");
+//     else process.stdout.write("  ");
+//   }
+//   console.log("\n");
+// }
+
+let n = Number(prompt("Enter the number of rows: "));
+for (let i = 1; i <= n; i++) {
+  for (let j = 1; j <= 2*n - 1; j++) {
+    if (i == j || i + j === 2*n) process.stdout.write("* ");
+    else process.stdout.write("  ");
+  } 
+    console.log("\n");
+}
