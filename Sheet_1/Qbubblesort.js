1ed428ab698eec6df418879b75a1c9c4044c9342
Rahul Madeshiya <rahulmadeshiya851@gmail.com>
2026-04-18 23:52:38 +0530

    sloving a bubble sort

---
+++ Sheet_1/Qbubblesort.js
@@ -0,0 +1,14 @@
+let arr = [5, 1, 4, 2, 8];
+let n = arr.length;
+for (let i = 0; i < n - 1; i++) {
+    for (let j = 0; j < n - 1 - i; j++) {
+        if (arr[j] > arr[j + 1]) {
+            // Swap arr[j] and arr[j+1]
+            let temp = arr[j];
+            arr[j] = arr[j + 1];
+            arr[j + 1] = temp;
+        }
+    }
+}
+
+console.log(arr);
