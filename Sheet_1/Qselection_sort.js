8145536c3951c168b58bf88558158e915f88106a
Rahul Madeshiya <rahulmadeshiya851@gmail.com>
2026-04-18 23:55:48 +0530

    solving a selectiopn sort

---
+++ Sheet_1/Qselection_sort.js
@@ -0,0 +1,16 @@
+let arr = [5, 1, 4, 2, 8];
+let n = arr.length;
+for (let i = 0; i < n - 1; i++) { 
+    let minIndex = i;
+    for (let j = i + 1; j < n; j++) {
+        if (arr[j] < arr[minIndex]) {
+            minIndex = j;
+        }
+    }
+    // Swap arr[i] and arr[minIndex]
+    let temp = arr[i];
+    arr[i] = arr[minIndex];
+    arr[minIndex] = temp;
+}
+
+console.log(arr);
