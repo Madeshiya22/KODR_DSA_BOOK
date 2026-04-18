7a830000cd7cc49c6e40a912f9ed58787a91a430
Rahul Madeshiya <rahulmadeshiya851@gmail.com>
2026-04-19 01:22:29 +0530

    solving a insertion sort

---
+++ Sheet_1/Qinsertion_sort.js
@@ -0,0 +1,12 @@
+let arr =[1,24,9,8,3,7]
+let n = arr.length;
+for(let i =0; i<n; i++){
+    let key = arr[i];
+    let j = i-1;
+    while(j>=0 && arr[j]>key){
+        arr[j+1] = arr[j]
+        j--;
+    }
+ arr[j+1] = key
+}
+console.log(arr)
--- Sheet_1/Qselection_sort.js
+++ Sheet_1/Qselection_sort.js
@@ -1,4 +1,4 @@
-let arr = [5, 1, 4, 2, 8];
+let arr = [10,5,12,1,3];
 let n = arr.length;
 for (let i = 0; i < n - 1; i++) { 
     let minIndex = i;
