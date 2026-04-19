b10f76b158c2f3c7acec75648e33f0302523afaa
Rahul Madeshiya <rahulmadeshiya851@gmail.com>
2026-04-20 00:10:50 +0530

    solving question

--- Sheet_1/Q_integer_to_word.js
+++ Sheet_1/Q_integer_to_word.js
@@ -1,57 +1,85 @@
-// 🔹 Global mappings
-const under_20 = ["", "One", "Two", "Three", "Four", "Five", "Six", "Seven",
-    "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen",
-    "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"];
+const under_20 = [
+  "",
+  "One",
+  "Two",
+  "Three",
+  "Four",
+  "Five",
+  "Six",
+  "Seven",
+  "Eight",
+  "Nine",
+  "Ten",
+  "Eleven",
+  "Twelve",
+  "Thirteen",
+  "Fourteen",
+  "Fifteen",
+  "Sixteen",
+  "Seventeen",
+  "Eighteen",
+  "Nineteen",
+];
 
-const tens = ["", "", "Twenty", "Thirty", "Forty", "Fifty",
-    "Sixty", "Seventy", "Eighty", "Ninety"];
+const tens = [
+  "",
+  "",
+  "Twenty",
+  "Thirty",
+  "Forty",
+  "Fifty",
+  "Sixty",
+  "Seventy",
+  "Eighty",
+  "Ninety",
+];
 
 // 🔹 Indian units
 const units = [
-    [1e7, "Crore"],
-    [1e5, "Lakh"],
-    [1e3, "Thousand"],
-    [1, ""]
+  [1e7, "Crore"],
+  [1e5, "Lakh"],
+  [1e3, "Thousand"],
+  [1, ""],
 ];
 
-// 🔹 Helper (0–999)
 function helper(n) {
-    if (n === 0) return "";
+  if (n === 0) return "";
 
-    if (n < 20) return under_20[n];
+  if (n < 20) return under_20[n];
 
     if (n < 100) {
-        return tens[Math.floor(n / 10)] +
-            (n % 10 !== 0 ? " " + under_20[n % 10] : "");
-    }
+    return (
+      tens[Math.floor(n / 10)] +
+      (n % 10 !== 0 ? " " + under_20[n % 10] : "")
+    );
+  }
 
-    return under_20[Math.floor(n / 100)] + " Hundred" +
-        (n % 100 !== 0 ? " " + helper(n % 100) : "");
+return (under_20[Math.floor(n / 100)] +
+    " Hundred" +
+    (n % 100 !== 0 ? " " + helper(n % 100) : "")
+);
 }
 
-// 🔹 Main function (while loop)
-function numberToWords(num) {
-    if (num === 0) return "Zero";
+function integerToWords(num) {
+  if (num === 0) return "ZERO";
+  let result = "";
+  let i = 0;
 
-    let result = "";
-    let i = 0;
+  while (i < units.length) {
+    let value = units[i][0];
+    let name = units[i][1];
 
-    while (i < units.length) {
-        let value = units[i][0];
-        let name = units[i][1];
+    if (num >= value) {
+      let current = Math.floor(num / value);
 
-        if (num >= value) {
-            let current = Math.floor(num / value);
-
-            if (current > 0) {
-                result += helper(current) + (name ? " " + name : "") + " ";
-                num %= value;
-            }
-        }
-
-        i++;
+      if (current > 0) {
+        result += helper(current) + (name ? " " + name : "") + " ";
+        num %= value;
+      }
     }
-
-    return result.trim();
+    i++;
+  }
+  return result.trim();
 }
-console.log(numberToWords(165000))
+
+console.log(integerToWords(12000));
