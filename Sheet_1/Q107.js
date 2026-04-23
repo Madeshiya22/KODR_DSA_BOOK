f3d9f4cff817f1b634d3e737cacb31e1404d8448
Rahul Madeshiya <rahulmadeshiya851@gmail.com>
2026-04-23 23:58:02 +0530

    sloving a reverse stfr

--- Sheet_1/Q107.js
+++ Sheet_1/Q107.js
@@ -1,7 +1,14 @@
 //Accept a string and print it in reverse order
+
 let str = "dohcrahdam";
 let reversedStr = "";
 for (let i = str.length - 1; i >= 0; i--) {
     reversedStr += str[i];
 }
 console.log(reversedStr);
+
+//otherway
+
+let str = "ranihc"
+let reverseStr = str.split("").reverse().join("");
+console.log(reversedStr)
